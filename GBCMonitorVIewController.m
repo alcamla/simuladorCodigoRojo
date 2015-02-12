9e23bc96f1d1c32dc1e2842add6a95de1a23ef20
FING140323 <fing140323@FING140323s-iMac-2.local>
2015-02-12 14:04:47 -0500

    Se agregó un nuevo sonido que representa el beep del monitor. El sonido se incluyó en una carpeta nueva llamada Sounds y fue descargado de un repositorio público de sonidos.

--- GBCMonitorVIewController.m
+++ GBCMonitorVIewController.m
@@ -28,6 +28,7 @@
 @property (strong, nonatomic) NSString *hoursString;
 @property (strong, nonatomic) NSString *minutesString;
 @property (strong, nonatomic) NSString *secondsString;
+@property (strong, nonatomic) NSSound *beepSound;
 
 
 @end
@@ -137,7 +138,10 @@ int minutes=0;
 - (void) updateChronometer{
     
     // Set a beep sound to the simulation
-    NSBeep();
+    self.beepSound=[NSSound soundNamed:@"ECGbeep.mp3"];
+    //self.beepSound=[NSSound soundNamed:@"Tink.aiff"];
+    [self.beepSound play];
+    //NSBeep();
     
     // Update Seconds, minutes and hours
     seconds++;
@@ -304,6 +308,14 @@ int minutes=0;
     }
     return _chronometer;
 }
+
+-(NSSound *)beepSound{
+    if (!_beepSound) {
+        _beepSound = [[NSSound alloc] init];
+    }
+    return _beepSound;
+}
+
 
 
 @end
